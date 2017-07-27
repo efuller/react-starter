1cdd97862b8f6af5dae2d72a0f4ecfc4d372c150
Eric Fuller <info@glassmoonmedia.com>
2017-07-27 07:48:36 -0400

    Initial commit.

---
+++ webpack/webpack.common.js
@@ -0,0 +1,29 @@
+const commonPaths = require('./webpack.paths');
+const ProgressBar = require('progress-bar-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+
+	entry: "./src/",
+	output: {
+		filename: 'bundle.js',
+		path: commonPaths.outputPath
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
+		]
+	},
+	plugins: [
+		new ProgressBar(),
+		new HTMLWebpackPlugin({
+			template: 'src/index.html'
+		}) // Builds and injects an HTML file into the dist folder.
+	]
+};
+
+module.exports = config;
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,16 @@
+const config = {
+	devtool: 'eval-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.css/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			}
+		]
+	}
+};
+
+module.exports = config;
