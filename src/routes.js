3c0c9307d4d33b1e92303a6e2e9c0fa4d7515997
Eric Fuller <info@glassmoonmedia.com>
2017-08-08 06:52:55 -0400

    Add in some routes using React Router.

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
+import Main from './components/Main';
+import LoginPage from './containers/LoginPage';
+import RegisterPage from './containers/RegisterPage';
+import DashboardPage from './containers/DashboardPage';
+import Playground from './containers/Playground';
+
+const Routes = () => (
+	<Switch>
+		<Route exact path="/" component={Main} />
+		<Route path="/login" component={LoginPage} />
+		<Route path="/register" component={RegisterPage} />
+		<Route path="/playground" component={Playground} />
+		<PrivateRoute path="/dashboard" component={DashboardPage} />
+	</Switch>
+);
+
+export default Routes;
