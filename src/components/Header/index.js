ba3bc3f2adc3716dc993ae884b27e00430ced36a
Eric Fuller <info@glassmoonmedia.com>
2017-09-08 07:53:18 -0400

    Rename files.

--- src/components/Header/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-	<header>Here is the Header!</header>
-);
-
-export default Header;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Provider from './utils/Provider';
 import App from './App';
 
+import './scss/styles.scss';
+
 ReactDOM.render(
 	<Provider>
 		<BrowserRouter>
