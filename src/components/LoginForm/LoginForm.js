79d92edb71ef533f12659819a15e12e5a2d6fd93
Eric Fuller <info@glassmoonmedia.com>
2017-09-13 08:14:41 -0400

    Login form can now handle local validation errors as well as errors from the server.

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import Validator from 'validator';
 import AuthService from '../../services/AuthService';
 
 class LoginForm extends Component {
@@ -8,52 +9,74 @@ class LoginForm extends Component {
 		super(props);
 
 		this.state = {
-			email: '',
-			password: '',
+			data: {
+				email: '',
+				password: ''
+			},
 			loggedIn: false,
-			userInfo: null
+			userInfo: null,
+			errors: {}
 		};
 
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleInputChange = this.handleInputChange.bind(this);
-	}
-
-	handleSubmit(e) {
-		e.preventDefault();
-		AuthService.login(this.state)
-			.then((res) => {
-				this.setState({ loggedIn: true, userInfo: res });
-				this.props.updateUserInfo({
-					userInfo: this.state.userInfo,
-					loggedIn: this.state.loggedIn
-				});
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+		this.validate = this.validate.bind(this);
 	}
 
 	handleInputChange(e) {
 		this.setState({
-			[e.target.name]: e.target.value
+			data: { ...this.state.data, [e.target.name]: e.target.value }
 		});
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const errors = this.validate(this.state.data);
+		this.setState({ errors });
+
+		if (Object.keys(errors).length === 0) {
+			AuthService.login(this.state.data)
+				.then((res) => {
+					this.setState({ loggedIn: true, userInfo: res });
+					this.props.updateUserInfo({
+						userInfo: this.state.userInfo,
+						loggedIn: this.state.loggedIn
+					});
+				})
+				.catch((err) => {
+					this.setState({ errors: err.response.data.errors });
+				});
+		}
+	}
+
+	validate(data) {
+		const errors = {};
+		if (!Validator.isEmail(data.email)) errors.email = 'Invalid Email';
+		if (!data.password) errors.password = 'Cannot be blank.';
+		return errors;
 	}
 
 	render() {
 		const from = this.props.location.state ? this.props.location.state.from.pathname : '/';
-		const { email, password, loggedIn } = this.state;
+		const { email, password } = this.state.data;
+		const { errors, loggedIn } = this.state;
 		return (
 			<div>
 				{loggedIn && (
 					<Redirect to={from || '/dashboard'} />
 				)}
+				{
+					<p>{errors.global}</p>
+				}
 				<form onSubmit={this.handleSubmit}>
 					<label htmlFor="email"> Email
 						<input type="text" id="email" name="email" className="full-width" value={email} onChange={this.handleInputChange} />
 					</label>
+					<p>{errors.email}</p>
 					<label htmlFor="password"> Password
 						<input type="password" id="password" name="password" className="full-width" value={password} onChange={this.handleInputChange} />
 					</label>
+					<p>{errors.password}</p>
 					<input type="submit" value="Login" />
 				</form>
 			</div>
