88249d43e0fb073066ba610f61a6953c9681e5fa
Eric Fuller <info@glassmoonmedia.com>
2017-09-12 20:22:32 -0400

    Chores.

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,11 +1,20 @@
 import Axios from 'axios';
 import setAuthorizationToken from '../utils/SetAuthorizationToken';
 
-const API_URL = process.env.API_URL || 'http://localhost:3001';
+const API_URL = process.env.API_URL;
+
+
+
 
 class AuthService {
+	static getAPIURL() {
+		if (process.env.NODE_ENV === 'production') {
+			return process.env.API_URL;
+		}
+		return 'http://localhost:3001';
+	}
 	static login(data) {
-		return Axios.post(`${API_URL}/auth/login`, data).then((res) => {
+		return Axios.post(`${AuthService.getAPIURL()}/auth/login`, data).then((res) => {
 			// Extract out the token.
 			const token = res.data.user.token;
 
@@ -22,7 +31,7 @@ class AuthService {
 
 	static getUserInfo(token) {
 		setAuthorizationToken(token);
-		return Axios.get(`${API_URL}/api/v1/user/me`).then((res) => {
+		return Axios.get(`${AuthService.getAPIURL()}/api/v1/user/me`).then((res) => {
 			return res.data.user;
 		});
 	}
